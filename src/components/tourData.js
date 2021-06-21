688cef7b4d5c84f2d7aede3941224dc7d0b07ec9
WalterOkumu <okumu.oriaro@gmail.com>
2021-06-21 16:36:42 +0300

    project completed

---
+++ src/components/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "nairobi",
+    img: "./img/nairobi.jpeg",
+    name: "city in the sun tour",
+    info:
+      "Alan Rails, ladies and gentlemen. After his parents' tragic death in a railroad accident, he gained the power to summon ghost trains. It's not all bad though, they were spared having to see their grown son wear a whistle."
+  },
+  {
+    id: 2,
+    city: "dar es salam",
+    img: "./img/daressalam.jpeg",
+    name: "dar es salam tour",
+    info:
+      "You're growing up fast, Morty. You're going into a great big thorn straight into my ass."
+  },
+  {
+    id: 3,
+    city: "addis ababa",
+    img: "./img/addis.jpeg",
+    name: "habesha tour",
+    info:
+      "Grassss... Taste bad!"
+  },
+  {
+    id: 4,
+    city: "kampala",
+    img: "./img/kampala.jpeg",
+    name: "pearl of africa",
+    info:
+      "Holy crap, Morty, run! Run for your life, Morty, run! I-I've never seen that thing before in my life, Morty, I don't even know what the hell it is! We-we gotta get out of here, Morty, it's gonna kill us! We're gonna die, Morty! We're gonna die!"
+  }
+];
